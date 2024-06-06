e91b6992906b49d120bd496a8fea454c47bdd57d
Nisan Shrestha <nisantheman@gmail.com>
2024-06-06 22:18:24 +0545

    fix text color, add utility class text-white

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function toggleLightMode() {
   if (toggle.checked) {
     console.log("light");
     document.documentElement.setAttribute('data-theme', 'light');
-    document.body.classList.add("light-mode");
+    // document.body.classList.add("light-mode");
     document.getElementsByClassName("toggle__cover")[0].classList.add("toggle__cover--left");
   }else{
     console.log("dark");
     document.documentElement.setAttribute('data-theme', 'dark');
-    document.body.classList.remove("light-mode");
+    // document.body.classList.remove("light-mode");
     document.getElementsByClassName("toggle__cover")[0].classList.remove("toggle__cover--left");
   }
 };
